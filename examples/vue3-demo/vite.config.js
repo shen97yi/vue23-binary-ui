cf245ccba6d1b9ac9d70fc6e4b8d67b09659be5c
徐鋂锋 <1956893126@qq.com>
2025-11-14 09:44:14 +0800

    打包vue3产物

--- examples/vue3-demo/vite.config.js
+++ examples/vue3-demo/vite.config.js
@@ -1,15 +1,6 @@
 import vue3 from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default { 
   plugins: [
-    vue3(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
+    vue3()
+  ]
 }
